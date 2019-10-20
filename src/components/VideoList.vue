4a3d04a248410228960069c0227753837251fe9d
Chris Andreotti <chris.andreotti@gmail.com>
2019-10-20 11:29:51 -0400

    Defining props and displaying them in child component

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,12 +1,16 @@
 <template>
     <ul>
         VideoList
+        {{ videos.length }}
     </ul>
 </template>
 
 <script>
 export default {
     name: 'VideoList',
+    props: {
+        videos: Array
+    }
 }
 </script>
 
